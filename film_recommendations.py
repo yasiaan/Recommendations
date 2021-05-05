a08c9bec6ef13d3622a3a79747af3546eb07c096
zouhriyassine <61051397+yasiaan@users.noreply.github.com>
2021-05-05 16:23:42 +0000

    Update film_recommendations.py

--- film_recommendations.py
+++ film_recommendations.py
@@ -16,7 +16,7 @@ class film_data():
         self.descriptions = []
         self.genres = []
         self.scrapped = 0
-        self.data = pd.DataFrame()
+        self.data = []
  
     def film_scrap(self):
 #         while self.url is not None:
@@ -52,7 +52,7 @@ class film_data():
         print(self.titles)
 
     def film_table(self):
-        if self.data == pd.DataFrame():
+        if len(self.data) == 0:
             self.film_scrap()
             imdb_films = pd.DataFrame()
             imdb_films['titles'] = self.titles
