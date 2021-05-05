ea0604e70bf0dbf3f58f093e63485bdef0c31406
zouhriyassine <61051397+yasiaan@users.noreply.github.com>
2021-05-05 17:04:07 +0000

    Update film_recommendations.py

--- film_recommendations.py
+++ film_recommendations.py
@@ -120,7 +120,7 @@ choosed = st.selectbox("Please Choose the movie for which you want recommendatio
 if st.button("Top 5 recommendation"):
     recommendations = sorted(list(enumerate(
         box.similarity[box.handler.get_index(choosed)])), key=lambda x: x[1], reverse=True)
-    st.hedear("The top 5 recommendations for" + " " + choosed + " " + "are: ")
+    st.header("The top 5 recommendations for" + " " + choosed + " " + "are: ")
     st.success(box.handler.get_title(recommendations[0][0]))
     st.success(box.handler.get_title(recommendations[1][0]))
     st.success(box.handler.get_title(recommendations[2][0]))
